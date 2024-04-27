ecbc0921dd85d6dc9d1baead7409a1ba175de7b7
Hadim anes <92207623+Anes-Hadim@users.noreply.github.com>
2024-04-27 16:25:13 +0000

    Add files via upload

---
+++ procedural-terrain-generation-main/src/intermediate-versions/sketch-v1.js
@@ -0,0 +1,23 @@
+/** This version has basic Perlin noise shown in grayscale. */
+
+let zoomFactor = 100;
+
+function setup() {
+  createCanvas(600, 600);
+  background(200);
+  noLoop();
+}
+
+function draw() {
+  for (x = 0; x < width; x++) {
+    for (y = 0; y < height; y++) {
+      // zoomFactor zooms in on the noise to make there be more steps between
+      // each plotted value. If zoomFactor is 100, we're effectively zooming in
+      // 100x on the noise map.
+      const noiseValue = noise(x / zoomFactor, y / zoomFactor);
+      set(x, y, color(255 * noiseValue));
+      //set(x, y, color(255 * Math.random()));
+    }
+  }
+  updatePixels();
+}
